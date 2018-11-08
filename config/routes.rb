353b6dbc12c51343dc491d3c369d749a9252425a
seanpara <spara77@gmail.com>
2018-11-08 11:20:07 -0500

    added controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :family_members, only: [:index, :show]
+      resources :currency_holdings, only: [:index, :show, :edit, :update]
+      resources :tangible_assets
+
+    end
+  end
 end
